30023f6ec5d9fb3905c06569580e18b712852bf8
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-01-24 05:58:26 +0530

    Create terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+sleep 2
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sleep 2
+sudo apt update && sudo apt install terraform -y
+terraform --version
