4c476ffb979c41da85c163373ad0ae1d139f9bc3
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-04-13 14:44:03 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -16,3 +16,8 @@ sleep 2
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt install docker-compose -y
 echo "Docker-compose is installed successfully"
+sleep 2
+echo "Adding ubuntu user to docker group"
+sudo usermod -aG docker ubuntu
+sudo newgrp docker
+echo "ubuntu added to docker group successfully"
